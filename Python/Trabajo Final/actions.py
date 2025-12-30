9a9a8f445525122d8b56279d0aaec4776bac898e
Frank <40705634+frankasg@users.noreply.github.com>
2025-12-30 14:52:23 -0600

    modulos de tarea de estudiantes creados (#2)
    * modulos de tarea de estudiantes creados
    
    * Actualización de regex para caracteres especiales

---
+++ Python/Trabajo Final/actions.py
@@ -0,0 +1,139 @@
+from common.operations_util import order_by_average
+from common.data_validations_util import student_exists
+
+def add_student_info(students : list, student_info : dict):
+    if student_exists(students, student_info["name"], student_info["section"]):
+        print(f"El estudiante {student_info["name"]} ya existe en la sección {student_info["section"]}")
+        return
+    
+    students.append(student_info)
+
+def print_all_students(students: list):
+    table_width = 105
+    print("\n" + "=" * table_width)
+    print("INFORMACiÓN DE ESTUDIANTES".center(table_width))
+    print("=" * table_width)
+
+    header = (
+        f"{"Nombre":<30}"
+        f"{"Sección":<10}"
+        f"{"Nota Español":<15}"
+        f"{"Nota Inglés":<15}"
+        f"{"Nota Sociales":<15}"
+        f"{"Nota Ciencias":<15}"
+    )
+    print(header)
+    print("-" * table_width)
+
+    for student in students:
+        print(
+            f"{student["name"]:<30}"
+            f"{student["section"]:<10}"
+            f"{student["spanish"]:<15}"
+            f"{student["english"]:<15}"
+            f"{student["social"]:<15}"
+            f"{student["science"]:<15}"
+        )
+
+    print("=" * table_width)
+
+def print_top3_students(students : list):
+    students_len = len(students) 
+    if students_len == 0:
+        print("Actualmente no hay registro de estudiantes")
+        return
+    top = min(3, students_len)
+    ordered_students = order_by_average(students)
+
+    table_width = 105
+    print("\n" + "=" * table_width)
+    print("TOP 3 DE ESTUDIANTES".center(table_width))
+    print("=" * table_width)
+
+    header = (
+        f"{"Nombre":<30}"
+        f"{"Sección":<10}"
+        f"{"Promedio":<15}"
+    )
+    print(header)
+    print("-" * table_width)
+
+    for i in range(top):
+        print(
+            f"{ordered_students[i]["name"]:<30}"
+            f"{ordered_students[i]["section"]:<10}"
+            f"{ordered_students[i]["average"]:<15}"            
+        )
+
+    print("=" * table_width)
+
+def print_general_average(students : list):
+    students_len = len(students)
+    if(students_len == 0):
+        print("Debe existir al menos 1 estudiante para poder mostrar el promedio general")
+        return
+    
+    general_average = 0
+    all_grades_total = 0
+
+    for student in students:
+        all_grades_total += student["average"] 
+    
+    general_average = all_grades_total / students_len
+
+    table_width = 105
+    print("\n" + "=" * table_width)
+    print(f"El promedio general entre todos los estudiantes es: {general_average:.2f}".center(table_width))
+    print("=" * table_width)
+
+def remove_student(students:list, name, section):    
+    student = {}
+    for s in students:
+        if s["name"] == name and s["section"] == section:
+            student = s    
+    
+    if student.get("name") is None:
+        print(f"El estudiante {name} en la sección {section} no existe")
+        return
+    
+    students.remove(student)
+    print(f"El estudiante {name} en la sección {section} ha sido eliminado del sistema")
+
+def print_failed_students(students: list):
+    if not any(s["spanish"] < 60 or s["english"] < 60 or s["social"] < 60 or s["science"] < 60 for s in students):
+        print("No hay estudiantes reprobados para mostrar en estos momentos")
+        return
+
+    table_width = 105
+    print("\n" + "=" * table_width)
+    print("ESTUDIANTES REPROBADOS".center(table_width))
+    print("=" * table_width)
+
+    header = (
+        f"{"Nombre":<30}"
+        f"{"Sección":<10}"
+        f"{"Nota Español":<15}"
+        f"{"Nota Inglés":<15}"
+        f"{"Nota Sociales":<15}"
+        f"{"Nota Ciencias":<15}"
+    )
+    print(header)
+    print("-" * table_width)
+
+    for student in students:
+        if student["spanish"] < 60 or student["english"] < 60 or student["social"] < 60 or student["science"] < 60 : 
+            spanish = student['spanish'] if student['spanish'] < 60 else "-"
+            english = student['english'] if student['english'] < 60 else "-"
+            social  = student['social']  if student['social']  < 60 else "-"
+            science = student['science'] if student['science'] < 60 else "-"
+
+            print(
+                f"{student['name']:<30}"
+                f"{student['section']:<10}"
+                f"{str(spanish):<15}"
+                f"{str(english):<15}"
+                f"{str(social):<15}"
+                f"{str(science):<15}"
+            )
+    print("=" * table_width)
+
---
+++ Python/Trabajo Final/main.py
@@ -0,0 +1,48 @@
+from menu import print_main_menu, get_menu_option, get_student_info, get_student_to_remove
+from actions import add_student_info, print_all_students, print_top3_students, print_general_average, remove_student, print_failed_students
+from data import export_csv, import_students_from_csv
+
+def main():    
+    try:
+        program_terminated = False
+        headers = ["name", "section", "spanish", "english", "social", "science"]
+        students = []
+
+        while not program_terminated:     
+            print_main_menu()
+            menu_option = get_menu_option()            
+            student_info = {}
+            
+            match menu_option:
+                case 1:
+                    get_student_info(student_info)
+                    add_student_info(students, student_info)
+                case 2:
+                    print_all_students(students)
+                case 3:
+                    print_top3_students(students)
+                case 4:
+                    print_general_average(students)
+                case 5:                    
+                    export_csv(students, headers)
+                case 6:
+                    import_result = import_students_from_csv()
+
+                    if import_result[0] == True:
+                        students = import_result[1]
+                case 7:
+                    student_to_remove = get_student_to_remove(students)
+
+                    if(student_to_remove[0] is not None):
+                        remove_student(students, student_to_remove[0], student_to_remove[1])
+                case 8:
+                    print_failed_students(students)
+                case 9:
+                    program_terminated = True
+                    continue                
+    
+    except Exception as ex:
+        print(f"Ha ocurrido un error inesperado. Detalle: {ex}")
+
+if __name__ == "__main__":
+    main()
