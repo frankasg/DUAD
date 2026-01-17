51264fa9a8fd38f87d0fe46804e942fdac440f9c
Frank <frankasg20@hotmail.com>
2026-01-17 00:12:33 -0600

    Revert "ejericios OOP"
    This reverts commit b29c74e8bdbc08a68828488b7d5938d6b06ab63d.

--- Python Intermedio/OOP/Ejercicio3/models/student.py
+++
@@ -1,9 +0,0 @@
-class Student:
-    def __init__(self, name, section, spanish, english, social, science, average):
-        self.name = name
-        self.section = section
-        self.spanish = spanish
-        self.english = english
-        self.social = social
-        self.science = science
-        self.average = average
